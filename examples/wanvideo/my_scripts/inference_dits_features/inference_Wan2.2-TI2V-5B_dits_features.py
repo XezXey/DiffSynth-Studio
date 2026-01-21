24d48c8d6fa2da6e4f0e809cc9a876e033d172c3
XezXey <puntawat-1@hotmail.com>
2026-01-22 00:10:28 +0700

    add

--- examples/wanvideo/my_scripts/inference_dits_features/inference_Wan2.2-TI2V-5B_dits_features.py
+++ examples/wanvideo/my_scripts/inference_dits_features/inference_Wan2.2-TI2V-5B_dits_features.py
@@ -8,13 +8,14 @@ from diffsynth.pipelines.wan_video import WanVideoPipeline, ModelConfig
 from diffsynth.motion_models.joint_map_vae import JointHeatMapMotionUpsample
 from modelscope import dataset_snapshot_download
 os.environ["DIFFSYNTH_MODEL_BASE_PATH"] = "/host/ist/ist-share/vision/huggingface_hub/"
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
 # # CPU Offload
 vram_config = {
-    "offload_dtype": torch.bfloat16,
+    "offload_dtype": torch.float8_e4m3fn,
     "offload_device": "cpu",
-    "onload_dtype": torch.bfloat16,
-    "onload_device": "cuda",
-    "preparing_dtype": torch.bfloat16,
+    "onload_dtype": torch.float8_e4m3fn,
+    "onload_device": "cpu",
+    "preparing_dtype": torch.float8_e4m3fn,
     "preparing_device": "cuda",
     "computation_dtype": torch.bfloat16,
     "computation_device": "cuda",
@@ -96,7 +97,7 @@ video, return_dict = pipe(
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
     seed=0, tiled=True, num_inference_steps=100,
     return_features=True,
-    height=352, width=624,
+    height=256, width=512,
     num_frames=61,
 )
 save_video(video, "video_1_Wan2.1-T2V-1.3B.mp4", fps=15, quality=5)
