b109714780fd8a3733d56699c86cfbcee8d8d67f
XezXey <puntawat-1@hotmail.com>
2026-01-22 00:18:33 +0700

    TODO: Preprocessing (Depth, Joint, Camera - Fixed), visulizing during training

--- examples/wanvideo/my_scripts/inference_dits_features/inference_Wan2.2-TI2V-5B_dits_features.py
+++ examples/wanvideo/my_scripts/inference_dits_features/inference_Wan2.2-TI2V-5B_dits_features.py
@@ -97,8 +97,8 @@ video, return_dict = pipe(
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
     seed=0, tiled=True, num_inference_steps=100,
     return_features=True,
-    height=256, width=512,
-    num_frames=61,
+    # height=256, width=512,
+    num_frames=81,
 )
 save_video(video, "video_1_Wan2.1-T2V-1.3B.mp4", fps=15, quality=5)
 
