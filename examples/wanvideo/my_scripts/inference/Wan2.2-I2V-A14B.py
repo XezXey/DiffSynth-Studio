4d520ece4da31c802e46a0a5a3a814f96c753651
XezXey <puntawat-1@hotmail.com>
2026-01-27 21:14:02 +0700

    add args

--- examples/wanvideo/my_scripts/inference/Wan2.2-I2V-A14B.py
+++ examples/wanvideo/my_scripts/inference/Wan2.2-I2V-A14B.py
@@ -5,6 +5,16 @@ from diffsynth.utils.data import save_video
 from diffsynth.pipelines.wan_video import WanVideoPipeline, ModelConfig
 from modelscope import dataset_snapshot_download
 os.environ["DIFFSYNTH_MODEL_BASE_PATH"] = "/host/ist/ist-share/vision/huggingface_hub/"
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_image", type=str, default="data/examples/wan/cat_fightning.jpg")
+parser.add_argument("--height", type=int, default=480)
+parser.add_argument("--width", type=int, default=832)
+parser.add_argument("--num_frames", type=int, default=121)
+parser.add_argument("--prompt", type=str, default="The girl in the input image walks straight forward naturally, with smooth and realistic walking motion. Her identity, clothing, hairstyle, and facial features remain unchanged. The camera is static, with no zoom, pan, or perspective change.")
+parser.add_argument("--negative_prompt", type=str, default="zoom, dolly, pan, tilt, camera movement, camera shake, perspective change, focal length change, background motion, parallax, cinematic camera, dynamic camera, motion blur, jitter, identity change, face distortion, body deformation, clothing change, inconsistent lighting")
+parser.add_argument("--save_suffix", type=str, default=None)
+args = parser.parse_args()
 
 vram_config = {
     "offload_dtype": "disk",
@@ -35,13 +45,18 @@ dataset_snapshot_download(
     local_dir="./",
     allow_file_pattern=["data/examples/wan/cat_fightning.jpg"]
 )
-input_image = Image.open("data/examples/wan/cat_fightning.jpg").resize((832, 480))
+input_image = Image.open(args.input_image).resize((args.width, args.height))
 
 video = pipe(
-    prompt="Two anthropomorphic cats in comfy boxing gear and bright gloves fight intensely on a spotlighted stage.",
-    negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
+    prompt=args.prompt,
+    negative_prompt=args.negative_prompt,
     seed=0, tiled=True,
     input_image=input_image,
     switch_DiT_boundary=0.9,
 )
-save_video(video, "video_Wan2.2-I2V-A14B.mp4", fps=15, quality=5)
+
+if args.save_suffix is None:
+    save_name = f"video_2_Wan2.2-I2V-A14B_{os.path.basename(args.input_image).split('.')[0]}.mp4"
+else:
+    save_name = f"video_2_Wan2.2-I2V-A14B_{os.path.basename(args.input_image).split('.')[0]}_{args.save_suffix}.mp4"
+save_video(video, save_name, fps=15, quality=5)
