b1f27bfc4d5323f723b3e3e5495dbe80c0cf1b88
XezXey <puntawat-1@hotmail.com>
2026-01-27 19:35:30 +0700

    convert rgb

--- examples/wanvideo/model_inference/Wan2.2-TI2V-5B.py
+++ examples/wanvideo/model_inference/Wan2.2-TI2V-5B.py
@@ -31,7 +31,7 @@ dataset_snapshot_download(
     local_dir="./",
     allow_file_pattern=["data/examples/wan/cat_fightning.jpg"]
 )
-input_image = Image.open("data/examples/wan/cat_fightning.jpg").resize((1248, 704))
+input_image = Image.open("data/examples/wan/cat_fightning.jpg").convert("RGB").resize((1248, 704))
 video = pipe(
     prompt="两只可爱的橘猫戴上拳击手套，站在一个拳击台上搏斗。",
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
