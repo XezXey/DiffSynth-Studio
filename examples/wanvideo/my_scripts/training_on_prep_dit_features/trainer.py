487adbec90644ad434e03b995825a3acdb9136eb
XezXey <puntawat-1@hotmail.com>
2026-02-18 12:37:03 +0000

    bac

--- examples/wanvideo/my_scripts/training_on_prep_dit_features/trainer.py
+++ examples/wanvideo/my_scripts/training_on_prep_dit_features/trainer.py
@@ -67,6 +67,77 @@ class TrainOnDiTFeatures(L.LightningModule):
         print("from process", th.distributed.get_rank())
         pass
     
+    def forward_pass(self, batch, batch_idx):
+        inputs = batch
+        dit_features = inputs["dit_features"].squeeze(0)  # B, C, H, W
+        inp = dit_features.type(th.float32)  # 1, #tokens, C
+        grid_size = inputs["grid_size"]
+        patch_size = inputs["patch_size"]
+
+        out_head = self.head(inp)  # 1, out_dim, H', W
+        # print(out_head.shape)
+        out_unpatched = self.unpatchify(out_head, grid_size, patch_size)  # 1, out_dim, T, H, W
+        # print(out_unpatched.shape)
+        out_decoded = self.joint_vae.decode(out_unpatched, device='cuda')  # 1, J*3, T, 1, 1
+        # print(out_decoded.shape)
+        out_joints_map = self.joint_head(out_decoded)  # 1, J*2, T, 1, 1
+        # print(out_joints_map.shape)
+
+        pixel_coords, depth = self.map_to_joint(out_joints_map)  # pixel_coords: (1, J, T, 2); depth: (1, J, T, 1)
+        # print(pixel_coords.shape, depth.shape)
+
+        # print(inputs["cams_intr"].shape)
+        # print(inputs["cams_extr"].shape)
+        fx, fy, cx, cy = inputs["cams_intr"].squeeze(0) # (4)
+        org_h = cy * 2.0 + 1
+        org_w = cx * 2.0 + 1
+        E_bl = inputs["cams_extr"].squeeze(0)  # (T, 4, 4)
+        # print(th.is_tensor(E_bl), E_bl.shape, E_bl.dtype, E_bl.ndim)
+
+        m3d_gt = inputs["joints_3d"].squeeze(0)  # T, J, 3
+        m2d_gt = inputs["joints_2d"].squeeze(0)  # T, J, 3
+        m2d_gt = m2d_gt[..., :2]    # only keep (u, v)
+        assert m3d_gt.shape[0] == m2d_gt.shape[0], "Number of frames mismatch between 3D and 2D joints."
+        assert m3d_gt.shape[1] == m2d_gt.shape[1], "Number of joints mismatch between 3D and 2D joints."
+        
+        
+        m2d_gt[..., 0] = m2d_gt[..., 0] / (org_w - 1)     # normalize to [0,1]
+        m2d_gt[..., 1] = m2d_gt[..., 1] / (org_h - 1)   # normalize to [0,1]
+        mask_2d = th.logical_and(m2d_gt >= 0.0, m2d_gt <= 1.0)
+        
+        h = inputs["height"]
+        w = inputs["width"]
+        u = pixel_coords[..., 0] * (org_w - 1)    # B, J, T
+        v = pixel_coords[..., 1] * (org_h - 1)    # B, J, T
+        d = depth[..., 0]
+        
+        motion_pred_2d = th.stack([u / (org_w - 1), v / (org_h - 1)], dim=-1).squeeze(0).permute(1, 0, 2)  # B, J, T -> T, J, 2
+        motion_pred_3d = self.unproject_torch(fx, fy, cx, cy, E_bl, th.stack([u, v, d], dim=-1).squeeze(0).permute(1, 0, 2))
+        training_target_3d = m3d_gt
+        assert motion_pred_3d.shape == training_target_3d.shape, f"motion_pred shape {motion_pred_3d.shape} does not match training_target shape {training_target_3d.shape}"
+        assert motion_pred_2d.shape == m2d_gt.shape, f"motion_pred_2d shape {motion_pred_2d.shape} does not match gt_motion_2d shape {m2d_gt.shape}"
+
+        loss_3d = th.nn.functional.mse_loss(motion_pred_3d.float(), training_target_3d.float())
+        loss_2d = th.nn.functional.mse_loss(motion_pred_2d.float(), m2d_gt.float()) * mask_2d.float()
+        loss_2d = loss_2d.sum() / (mask_2d.float().sum() + 1e-8)
+        loss = loss_3d + loss_2d * 10.0
+
+        output_dict = {
+            "motion_pred_3d": motion_pred_3d.detach().cpu(),
+            "motion_gt_3d": training_target_3d.detach().cpu(),
+            "motion_pred_2d": motion_pred_2d.detach().cpu(),
+            "motion_gt_2d": m2d_gt.detach().cpu(),
+            "loss_3d": loss_3d.item(),
+            "loss_2d": loss_2d.item(),
+            "joint_names": inputs["joint_names"],
+            "bones": inputs["bones"]
+        }
+        loss_dict = {"loss": loss, "loss_3d": loss_3d, "loss_2d": loss_2d}
+
+
+        return loss_dict, output_dict
+
+
     def training_step(self, batch, batch_idx):
         """
         batch contains the following keys:
@@ -76,15 +147,10 @@ class TrainOnDiTFeatures(L.LightningModule):
 
         """
         inputs = batch
-        # for k in inputs.keys():
-        #     if isinstance(inputs[k], th.Tensor):
-        #         print(f"{k}: shape {inputs[k].shape}, dtype {inputs[k].dtype}, device {inputs[k].device}")
-        # exit()
         dit_features = inputs["dit_features"].squeeze(0)  # B, C, H, W
         inp = dit_features.type(th.float32)  # 1, #tokens, C
         grid_size = inputs["grid_size"]
         patch_size = inputs["patch_size"]
-        # print(grid_size, patch_size)
 
         out_head = self.head(inp)  # 1, out_dim, H', W
         # print(out_head.shape)
